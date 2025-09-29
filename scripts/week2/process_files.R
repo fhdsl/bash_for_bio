3d5caa47c5cd875586233be0ea83fb539d379199
Ted Laderas <roshioshi@gmail.com>
2025-09-29 13:25:00 -0700

    adding csv files and edits

--- scripts/week2/process_files.R
+++ scripts/week2/process_files.R
@@ -4,10 +4,10 @@ library(janitor)
 args <- commandArgs()
 
 # Use arg$CSVFILE in read.csv
-csv_file <- read.csv(file=args$input_file) #<1>
+csv_file <- read.csv(file=args$input_file)
 
 # Do some work with csv_file
 csv_filtered <- csv_file |> janitor::clean_names()
 
 # Write output
-write.csv(csv_filtered, file = paste0(args$CSVFILE, "_filtered.csv")) #<2>
+write.csv(csv_filtered, file = paste0(args$CSVFILE, "_cleaned.csv"))
