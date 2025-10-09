93051bffbbbe4a7072988e04edd88dbef552a589
Ted Laderas <roshioshi@gmail.com>
2025-10-09 08:21:08 -0700

    revisions

--- scripts/week3/run_sbatch.sh
+++ scripts/week3/run_sbatch.sh
@@ -3,9 +3,11 @@
 #SBATCH --array=1-3
 #SBATCH --mem-per-cpu=1gb
 #SBATCH --time=00:10:00
-file_array=(../../data/*.fastq)
+#SBATCH --mail-user=tladera2@fredhutch.org
+file_array=(./data/*.fastq)
 ind=$((SLURM_ARRAY_TASK_ID-1))
 echo "$ind"
 current_file=${file_array[$ind]}
 echo "$current_file"
-./run_bwa.sh $current_file
+# execute the `run_bwa.sh` script on $current_file
+./run_bwa.sh "$current_file"
