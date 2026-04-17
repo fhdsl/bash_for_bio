7b83b2df293077202a385f1f3d731d6f9e3a8c78
Ted Laderas <roshioshi@gmail.com>
2026-04-17 10:12:36 -0700

    updating week1 python script

--- scripts/week1/process_files.py
+++ scripts/week1/process_files.py
@@ -2,4 +2,5 @@ import sys #<1>
 import pandas as pd
 file = sys.argv[1] #<2> 
 df = pd.read_csv(file)
-print(df.tumor_stage.nunique())
+summary = df.tumor_stage.value_counts()
+summary.to_csv(file+".summary.csv")
